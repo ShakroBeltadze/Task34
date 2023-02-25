0acc1876f60d80e0da2d58b375a4cb521d6df656
Shakro.Beltadze <shakro@bk.ru>
2023-02-25 17:46:05 +0300

    Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.

---
+++ Seminar5/001/Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+int Size;
+
+do
+{
+    Console.Write("Введите размер массива:  ");
+    Size = Convert.ToInt32(Console.ReadLine());
+}
+while (Size <= 0);
+
+int[] array = new int[Size];
+
+
+int PrintArray(int[] intArray)
+{
+    Console.Write("[");
+    for (int i = 0; i < intArray.Length; i++)
+        if (i < Size - 1)
+        {
+            Console.Write(array[i] + ", ");
+        }
+        else
+        {
+            Console.Write(array[i]);
+        }
+    Console.WriteLine("]");    
+    return 1;
+}
+
+int CreatArray(int[] intArray)
+{
+    for (int i = 0; i < intArray.Length; i++)
+        array[i] = new Random().Next(100, 1001);
+    return 1;
+}
+
+int EvenNumber(int[] intArray)
+{
+    int count=0;
+    for (int i = 0; i < intArray.Length; i++)
+        if (array[i]%2==0)
+        {
+            count++;
+        } 
+    return count;
+}
+
+
+
+CreatArray(array);
+
+PrintArray(array);
+
+Console.WriteLine($"Количество чётных чисел в массиве: {EvenNumber(array)}");  
